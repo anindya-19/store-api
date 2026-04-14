1046b1d1241ac3fa37cf008faa699f73ce673721
Anindya Ganguly <anindyaganguly19@gmail.com>
2026-04-14 22:04:51 +0530

    feat: implement dynamic product filtering with regex search and query object validation

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,31 @@
 const Products = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const product = await Products.find({ name: "armchair" });
-  res.status(200).json({ product, nbHits: product.length });
+  const search = "wooden";
+  const product = await Products.find({
+    name: { $regex: search, $options: "i" },
+  });
+  res.status(200).json({ nbHits: product.length, product });
 };
+
+//all products dynamic rendering
 const getAllProducts = async (req, res) => {
   console.log(req.query); //{ name: 'john', featured: 'true' }
-  const { featured } = req.query;
-  const products = await Products.find(req.query); //directly passing the query object
-  res.status(200).json({ products });
+  const { featured, company, name } = req.query; //ony pulling out the featured and company out of the query
+
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false; //set a featured property to the query object
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  console.log(queryObject);
+  const products = await Products.find(queryObject); //directly passing the query object
+  res.status(200).json({ nbHits: products.length, products });
 };
 
 module.exports = {
